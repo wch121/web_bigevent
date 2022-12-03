c9e57094493033d2ed3403f6d44bf802366a0cee
wch <1165553576@qq.com>
2022-12-04 02:28:40 +0800

    增加了筛选文章功能

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,10 +1,10 @@
-$(function() {
+$(function () {
   var layer = layui.layer
   var form = layui.form
   var laypage = layui.laypage
 
   // 定义美化时间的过滤器
-  template.defaults.imports.dataFormat = function(date) {
+  template.defaults.imports.dataFormat = function (date) {
     const dt = new Date(date)
 
     var y = dt.getFullYear()
@@ -27,7 +27,7 @@ $(function() {
   // 需要将请求参数对象提交到服务器
   var q = {
     pagenum: 1, // 页码值，默认请求第一页的数据
-    pagesize: 2, // 每页显示几条数据，默认每页显示2条
+    pagesize: 10, // 每页显示几条数据，默认每页显示10条
     cate_id: '', // 文章分类的 Id
     state: '' // 文章的发布状态
   }
@@ -41,7 +41,7 @@ $(function() {
       method: 'GET',
       url: '/my/article/list',
       data: q,
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('获取文章列表失败！')
         }
@@ -59,7 +59,7 @@ $(function() {
     $.ajax({
       method: 'GET',
       url: '/my/article/cates',
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('获取分类数据失败！')
         }
@@ -73,7 +73,7 @@ $(function() {
   }
 
   // 为筛选表单绑定 submit 事件
-  $('#form-search').on('submit', function(e) {
+  $('#form-search').on('submit', function (e) {
     e.preventDefault()
     // 获取表单中选中项的值
     var cate_id = $('[name=cate_id]').val()
@@ -94,12 +94,12 @@ $(function() {
       limit: q.pagesize, // 每页显示几条数据
       curr: q.pagenum, // 设置默认被选中的分页
       layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
-      limits: [2, 3, 5, 10],
+      limits: [2, 4, 5, 10,20],
       // 分页发生切换的时候，触发 jump 回调
       // 触发 jump 回调的方式有两种：
       // 1. 点击页码的时候，会触发 jump 回调
       // 2. 只要调用了 laypage.render() 方法，就会触发 jump 回调
-      jump: function(obj, first) {
+      jump: function (obj, first) {
         // 可以通过 first 的值，来判断是通过哪种方式，触发的 jump 回调
         // 如果 first 的值为 true，证明是方式2触发的
         // 否则就是方式1触发的
@@ -119,18 +119,18 @@ $(function() {
   }
 
   // 通过代理的形式，为删除按钮绑定点击事件处理函数
-  $('tbody').on('click', '.btn-delete', function() {
+  $('tbody').on('click', '.btn-delete', function () {
     // 获取删除按钮的个数
     var len = $('.btn-delete').length
     console.log(len)
     // 获取到文章的 id
     var id = $(this).attr('data-id')
     // 询问用户是否要删除数据
-    layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
+    layer.confirm('确认删除?', { icon: 3, title: '提示' }, function (index) {
       $.ajax({
         method: 'GET',
         url: '/my/article/delete/' + id,
-        success: function(res) {
+        success: function (res) {
           if (res.status !== 0) {
             return layer.msg('删除文章失败！')
           }
